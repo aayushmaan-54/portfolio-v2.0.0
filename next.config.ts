459bf68e5d4e4bb8a0c8ffca3df9703e605d14b5
Aayushmaan Soni <aayushmaan.soni54@gmail.com>
2025-07-13 21:27:09 +0530

    Fix Metadata issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  htmlLimitedBots: /.*/,
+  htmlLimitedBots: /Mediapartners-Google|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview/i,
 };
 
 export default nextConfig;
